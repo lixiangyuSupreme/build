d4a2e7935400c2c81ba056f8d15ecc96c60fc7bd
李项宇 <45160965+lixiangyuSupreme@users.noreply.github.com>
2020-10-04 16:31:30 +0800

    Add files via upload

---
+++ build/precache-manifest.41c16b9986b3652b252767a2def78981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8a103276a0253d702f12acbcb4302e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e15bae3b853e7e76fe",
+    "url": "/static/js/main.87fd7db1.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
